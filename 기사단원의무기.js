b6125bd2d05d46387ad803c908dd865ff8222ed3
hansejun <tpwnszja2@gmail.com>
2022-12-21 23:29:14 +0900

    Update 기사단원의무기.js
    조금 더 최적화

--- 기사단원의무기.js
+++ 기사단원의무기.js
@@ -1,46 +1,45 @@
-// 나의 코드 ( 100만 기준 성능 40%  이상 차이)
-// function solution(number, limit, power) {
-//   const set = new Set(
-//     Array(number)
-//       .fill(1)
-//       .map((num, idx) => num + idx)
-//   );
-//   const arr = [];
-//   let initial = 1;
-//   while (initial <= number) {
-//     if (set.has(initial)) {
-//       const cnt = getYaksu(initial, limit);
-//       if (cnt > limit) {
-//         for (let i = 1; initial * i <= number; i++) {
-//           set.delete(initial * i);
-//         }
-//       } else {
-//         arr.push(cnt);
-//       }
-//     }
-//     initial++;
-//   }
-//   const result = arr.reduce((a, b) => a + b);
-//   const gap = number - arr.length;
-//   return gap === 0 ? result : result + gap * power;
-// }
+// // 나의 코드 ( 1000만 기준 13s)
+function solution(number, limit, power) {
+  const set = new Set(
+    Array(number)
+      .fill(1)
+      .map((num, idx) => num + idx)
+  );
+  let result = 0;
+  let initial = 1;
+  while (initial <= number) {
+    if (set.has(initial)) {
+      const cnt = getYaksu(initial, limit);
+      if (cnt > limit) {
+        for (let i = 1; initial * i <= number; i++) {
+          set.delete(initial * i);
+        }
+      } else {
+        result += cnt;
+      }
+    }
+    initial++;
+  }
+  const gap = number - set.size;
+  return gap === 0 ? result : result + gap * power;
+}
 
-// // 약수 개수 알고리즘
-// function getYaksu(num, limit) {
-//   let cnt = 0;
-//   for (let i = 1; i <= Math.sqrt(num); i++) {
-//     if (num % i == 0) {
-//       cnt++;
-//       if (i * i < num) {
-//         cnt++;
-//       }
-//     }
-//     if (cnt > limit) break;
-//   }
-//   return cnt;
-// }
+// 약수 개수 알고리즘
+function getYaksu(num, limit) {
+  let cnt = 0;
+  for (let i = 1; i <= Math.sqrt(num); i++) {
+    if (num % i == 0) {
+      cnt++;
+      if (i * i < num) {
+        cnt++;
+      }
+    }
+    if (cnt > limit) break;
+  }
+  return cnt;
+}
 
-// 우수코드
+//우수코드 (1000만 기중 30s)
 // function solution(number, limit, power) {
 //   var answer = 0;
 //   for (let n = 1; n <= number; n++) {
@@ -56,5 +55,5 @@
 // }
 
 console.time("solution");
-console.log(solution(1000000, 6, 2));
+console.log(solution(10000000, 6, 2));
 console.timeEnd("solution");
